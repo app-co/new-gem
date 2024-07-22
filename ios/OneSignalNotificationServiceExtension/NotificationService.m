820124d32f4a17d9015c499e8c5e8fdc7000410b
app-co <william@appcom.dev>
2024-07-22 11:01:57 -0300

    .

--- ios/OneSignalNotificationServiceExtension/NotificationService.m
+++ ios/OneSignalNotificationServiceExtension/NotificationService.m
@@ -1,4 +1,4 @@
-#import <OneSignalFramework/OneSignalFramework.h>
+#import <OneSignalExtension/OneSignalExtension.h>
 
 #import "NotificationService.h"
 
@@ -16,13 +16,24 @@
     self.receivedRequest = request;
     self.contentHandler = contentHandler;
     self.bestAttemptContent = [request.content mutableCopy];
-    [OneSignal didReceiveNotificationExtensionRequest:self.receivedRequest
+    
+    /* DEBUGGING: Uncomment the 2 lines below and comment out the one above to ensure this extension is executing
+                  Note, this extension only runs when mutable-content is set
+                  Setting an attachment or action buttons automatically adds this */
+    // NSLog(@"Running NotificationServiceExtension");
+    // self.bestAttemptContent.body = [@"[Modified] " stringByAppendingString:self.bestAttemptContent.body];
+    
+    [OneSignalExtension didReceiveNotificationExtensionRequest:self.receivedRequest
                        withMutableNotificationContent:self.bestAttemptContent
                                    withContentHandler:self.contentHandler];
 }
 
 - (void)serviceExtensionTimeWillExpire {
-    [OneSignal serviceExtensionTimeWillExpireRequest:self.receivedRequest withMutableNotificationContent:self.bestAttemptContent];
+    // Called just before the extension will be terminated by the system.
+    // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+    
+    [OneSignalExtension serviceExtensionTimeWillExpireRequest:self.receivedRequest withMutableNotificationContent:self.bestAttemptContent];
+    
     self.contentHandler(self.bestAttemptContent);
 }
 
